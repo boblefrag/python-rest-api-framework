e52cef7619763f0e86a6c8be5a2c8fb944302c89
Yohann Gabory <yohann.gabory@outscale.com>
2013-10-08 00:19:23 +0200

    use nose for unit testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,1 @@
-import unittest
 
-from datastore_test import PythonListDataStoreTest
-from apiview_test import TestApiView
-
-
-__all__ = [PythonListDataStoreTest, TestApiView]
-
-if __name__ == "__main__":
-
-    unittest.main()

ab4842bd3f7e47ba69c9db02106266222b2aaf22
Yohann Gabory <yohann.gabory@outscale.com>
2013-10-08 01:31:00 +0200

    removed WSGIWrapper to the view module.

--- rest_api_framework/base.py
+++ rest_api_framework/base.py
@@ -3,7 +3,7 @@ The minimum needed to take a response and render a response
 - url mapper utility
 - wsgiwrapper
 """
-from werkzeug.wrappers import Request, Response
+from werkzeug.wrappers import Response
 
 
 class JsonResponse(Response):
@@ -15,16 +15,3 @@ class JsonResponse(Response):
                      self).__init__(*args,
                                     mimetype="application/json",
                                     **kwargs)
-
-
-class WSGIWrapper(object):
-    """
-    accept a request, return a response
-    """
-    def __call__(self, environ, start_response):
-        return self.wsgi_app(environ, start_response)
-
-    def wsgi_app(self, environ, start_response):
-        request = Request(environ)
-        response = self.dispatch_request(request)
-        return response(environ, start_response)

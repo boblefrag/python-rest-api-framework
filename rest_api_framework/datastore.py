e53c5760bb699a910d36b953c497f9029da25aa2
Yohann Gabory <yohann.gabory@outscale.com>
2013-10-08 16:17:41 +0200

    added the sqldatastore

--- rest_api_framework/datastore.py
+++ rest_api_framework/datastore.py
@@ -1,4 +1,5 @@
 from werkzeug.exceptions import BadRequest, NotFound
+import sqlite3
 
 
 class DataStore(object):
@@ -6,6 +7,14 @@ class DataStore(object):
     define a source of data. Can be anything fron database to other
     api, files and so one
     """
+
+    def __init__(self, data, **options):
+        """
+        Set the ressource datastore
+        """
+        self.data = data
+        self.options = options
+        self.description = options["description"]
 
     def paginate(self, data, **kwargs):
         """
@@ -33,7 +42,7 @@ class DataStore(object):
                             )
         """
         start = 0
-        end = self.options['paginate_by']
+        end = self.options.get('paginate_by', None)
         if "start" in kwargs:
             start = int(kwargs['start'])
             end = start + self.options['paginate_by']
@@ -75,14 +84,6 @@ class PythonListDataStore(DataStore):
     """
     a datastore made of list of dicts
     """
-
-    def __init__(self, data, **options):
-        """
-        Set the ressource datastore
-        """
-        self.data = data
-        self.options = options
-        self.description = options["description"]
 
     def get(self, identifier):
         """
@@ -147,3 +148,126 @@ class PythonListDataStore(DataStore):
     def delete(self, identifier):
         obj = self.get(identifier)
         self.data.remove(obj)
+
+
+class SQLDataStore(DataStore):
+
+    wrapper = {int: "integer",
+               float: "real",
+               basestring: "text",
+               "autoincrement": "integer primary key autoincrement"
+               }
+
+    def __init__(self, data, **options):
+        conn = sqlite3.connect(data["name"])
+        c = conn.cursor()
+        table = data["table"]
+        super(SQLDataStore, self).__init__({"conn": conn, "table": table},
+                                           **options)
+        fields = ", ".join(
+            ["{0} {1}".format(k,
+                              self.wrapper[v['type']]
+                              ) for k, v in self.description.iteritems()])
+        sql = 'create table if not exists {0} ({1})'.format(table, fields)
+        c.execute(sql)
+        conn.commit()
+
+    def filter(self, **kwargs):
+        kwargs['query'] += ' FROM {0}'
+        return kwargs
+
+    def paginate(self, data, **kwargs):
+
+        limit = self.options.get('paginate_by', None)
+        if kwargs.get("start", None):
+            data["query"] += " WHERE id >= {0}".format(kwargs["start"])
+
+        if kwargs.get("end", None):
+            data["query"] += " WHERE id < {0} order by id DESC".format(
+                kwargs["end"])
+        if limit:
+            data["query"] += " LIMIT {0}".format(limit)
+
+        print data["query"]
+
+        c = self.data['conn'].cursor()
+        c.execute(data["query"].format(self.data['table']))
+        objs = []
+        for elem in c.fetchall():
+            fields = [k for k in self.description.iterkeys()]
+            objs.append(dict(zip(fields, elem)))
+        if kwargs.get("end", None):
+            objs.reverse()
+        return objs
+
+    def get_list(self, **kwargs):
+        """
+        return all the objects, paginated if needed
+        """
+        fields = ", ".join([k for k in self.description.iterkeys()])
+        kwargs["query"] = 'SELECT {0}'.format(fields)
+        start = kwargs.pop("start", None)
+        end = kwargs.pop("end", None)
+        data = self.filter(**kwargs)
+        return self.paginate(data, start=start, end=end)
+
+    def get(self, identifier):
+
+        c = self.data['conn'].cursor()
+        fields = ", ".join([k for k in self.description.iterkeys()])
+        query = "select {0} from {1} where id=?".format(
+            fields,
+            self.data["table"])
+        c = self.data['conn'].cursor()
+        c.execute(query, (identifier,))
+        obj = c.fetchone()
+        if obj:
+            fields = [k for k in self.description.iterkeys()]
+            return dict(zip(fields, obj))
+        else:
+            raise NotFound
+
+    def create(self, data):
+        self.validate(data)
+        fields = []
+        values = []
+        for k, v in data.iteritems():
+            if k in self.description.iterkeys():
+                fields.append(k)
+                values.append(v)
+        c = self.data['conn'].cursor()
+        query = "insert into {0} {1} values {2}".format(
+            self.data["table"],
+            tuple(fields),
+            tuple(values))
+        c.execute(query)
+        return c.lastrowid
+        # qs = " and ".join(["{0}={1}".format(f, v) for f, v in zip(fields, values)])
+        # query = "select id from {0} where {1}".format(self.data["table"],
+        #                                               qs)
+        # print query
+
+    def update(self, obj, data):
+        self.get(obj['id'])
+        self.validate_fields(data)
+        fields = []
+        values = []
+        for k, v in data.iteritems():
+            if k in self.description.iterkeys():
+                fields.append(k)
+                values.append(v)
+        c = self.data['conn'].cursor()
+        update = " ".join(["{0}='{1}'".format(f, v) for f, v in zip(fields, values)])
+        query = "update {0} set {1}".format(
+            self.data["table"],
+            update
+            )
+        c.execute(query)
+        return self.get(obj['id'])
+
+    def delete(self, identifier):
+        obj = self.get(identifier)
+        c = self.data['conn'].cursor()
+        query = "delete from {0} where id={1}".format(self.data["table"],
+                                                      identifier)
+        c.execute(query)
--- tests/datastore_test.py
+++ tests/datastore_test.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
-from rest_api_framework.datastore import PythonListDataStore, DataStore
+from rest_api_framework.datastore import (PythonListDataStore,
+                                          SQLDataStore)
 from werkzeug.exceptions import BadRequest, NotFound
+
 
 options = {
     "description": {
@@ -144,3 +146,105 @@ class PythonListDataStoreTest(TestCase):
         self.assertEqual(len(store.filter(name="bob")), 100)
         self.assertEqual(len(store.filter(name="john")), 0)
         self.assertEqual(len(store.filter(name="bob", age=12)), 1)
+
+
+class SQLDataStoreTest(TestCase):
+
+    def test_validation(self):
+        store = SQLDataStore(
+            {"name": "test_db", "table": "address"},
+            **options)
+        self.assertEqual(store.validate({"name": "bob", "age": 34}), None)
+        self.assertRaises(BadRequest, store.validate, "a test")
+        self.assertRaises(BadRequest,
+                          store.validate,
+                          {"name": "bob", "age": "34"})
+        self.assertRaises(BadRequest,
+                          store.validate_fields,
+                          {"age": "34"})
+        self.assertRaises(BadRequest,
+                          store.validate_fields,
+                          "age")
+
+    def test_pagination(self):
+        store = SQLDataStore(
+            {"name": "test.db", "table": "address"},
+            paginate_by=10,
+            **options)
+        for i in range(100):
+            store.create({"name": "bob", "age": 34})
+        self.assertEqual(len(store.get_list()), 10)
+        self.assertEqual(store.get_list(start=10)[0]["id"], 10)
+        self.assertEqual(store.get_list(end=15)[-1]["id"], 14)
+
+    def test_get(self):
+
+        store = SQLDataStore(
+            {"name": "test.db", "table": "address"},
+            **options)
+
+        for i in range(100):
+            store.create({"name": "bob", "age": 34})
+
+        self.assertEqual(store.get(10)["id"], 10)
+        self.assertRaises(NotFound,
+                          store.get,
+                          101)
+
+    def test_create(self):
+
+        store = SQLDataStore(
+            {"name": "test.db", "table": "address"},
+            **options)
+
+        self.assertRaises(NotFound,
+                          store.get,
+                          1)
+        self.assertEqual(store.create({"name": "bob", "age": 34}), 1)
+        self.assertEqual(store.create({"name": "bob", "age": 35}), 2)
+        self.assertEqual(store.create({"name": "bob", "age": 34}), 3)
+        print store.get(3)
+        self.assertEqual(store.get(3)["id"], 3)
+
+    def test_update(self):
+        store = SQLDataStore(
+            {"name": "test.db", "table": "address"},
+            **options)
+
+        for i in range(100):
+            store.create({"name": "bob", "age": 34})
+
+        self.assertEqual(
+            store.update(
+                {"name": "bob", "age": 34, "id": 34},
+                {"name": "boby mc gee"}
+                ),{"name": "boby mc gee", "age": 34, "id": 34})
+        # adress is not part of the ressource description, it should
+        # raise
+        self.assertRaises(BadRequest,
+                          store.update,
+                          {"name": "bob", "age": "35", "id": 35},
+                          {"adress": "1, main street"}
+                          )
+
+        self.assertRaises(NotFound,
+                          store.update,
+                          {"name": "bob", "age": "100", "id": 400},
+                          {"name": "boby mc gee"}
+                          )
+
+    def test_delete(self):
+        store = SQLDataStore(
+            {"name": "test.db", "table": "address"},
+            **options)
+
+        for i in range(100):
+            store.create({"name": "bob", "age": 34})
+        # the object exists
+        self.assertEqual(store.get(10)["id"], 10)
+        # is delete
+        self.assertEqual(store.delete(10), None)
+        # does not exist anymore
+        self.assertRaises(NotFound,
+                          store.get,
+                          10)

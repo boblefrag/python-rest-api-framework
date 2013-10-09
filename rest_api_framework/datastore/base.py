e788c1dc2b657f7cd69fab329916940b0d24d641
Yohann Gabory <yohann.gabory@outscale.com>
2013-10-09 08:36:45 +0200

    updated the documentation

--- rest_api_framework/datastore/base.py
+++ rest_api_framework/datastore/base.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from werkzeug.exceptions import BadRequest
 
 
@@ -15,30 +17,87 @@ class DataStore(object):
         self.options = options
         self.description = options["description"]
 
+    def get(self, identifier):
+        """
+        Should return a dictionnary representing the ressource matching the
+        identifier or raise a NotFound exception.
+
+        .. note::
+
+           Not implemented by base DataStore class
+        """
+        raise NotImplemented
+
+    def create(self, data):
+        """
+        data is a dict containing the representation of the
+        ressource. This method should call
+        :meth:`~.DataStore.validate`,
+        create the data in the datastore and return the ressource
+        identifier
+
+        .. note::
+
+           Not implemented by base DataStore class
+        """
+        raise NotImplemented
+
+    def update(self, obj, data):
+        """
+        should be able to call :meth:`~.DataStore.get` to retreive the
+        object to be updated, :meth:`~.DataStore.validate_fields` and
+        return the updated object
+
+        .. note::
+
+           Not implemented by base DataStore class
+        """
+        raise NotImplemented
+
+    def delete(self, identifier):
+        """
+        should be able to validate the existence of the object in the
+        ressource and remove it from the datastore
+
+        .. note::
+
+           Not implemented by base DataStore class
+        """
+        raise NotImplemented
+
+    def get_list(self, **kwargs):
+        """
+        This method is called each time you want a set of data.
+        Data could be paginated and filtered.
+        Should call :meth:`~.DataStore.filter`
+        and return :meth:`~.DataStore.paginate`
+
+        .. note::
+
+           Not implemented by base DataStore class
+        """
+        raise NotImplemented
+
+    def filter(self, **kwargs):
+        """
+        should return a way to filter the ressource according to
+        kwargs.  It is not mandatory to actualy retreive the
+        ressources as they will be paginated just after the filter
+        call. If you retreive the wole filtered ressources you loose
+        the pagination advantage. The point here is to prepare the
+        filtering. Look at SQLiteDataStore.filter for an example.
+
+        .. note::
+
+           Not implemented by base DataStore class
+        """
+        raise NotImplemented
+
     def paginate(self, data, **kwargs):
         """
-        Naive implementation of pagination. Ressource_list must implement
-        indexing in a clever way. This naive implementation does not check
-        for negative index nor outbound index.
-        HINT for python generator:
-
-        import itertools
-        class Indexable(object):
-        def __init__(self,it):
-            self.it=it
-        def __iter__(self):
-            for elt in self.it:
-                yield elt
-        def __getitem__(self,index):
-            try:
-                return next(itertools.islice(self.it,index,index+1))
-            except TypeError:
-                return list(itertools.islice(self.it,
-                                             index.start,
-                                             index.stop,
-                                             index.step
-                                             )
-                            )
+        Paginate sould return all the object if no pagination options
+        have been set or only a subset of the ressources if pagination
+        options exists.
         """
         start = 0
         end = self.options.get('paginate_by', None)
@@ -53,7 +112,11 @@ class DataStore(object):
 
     def validate(self, data):
         """
-        Check if data send are valide for objec creation
+        Check if data send are valide for objec creation. Validate
+        Chek that each required fields are in data and check for their
+        type too.
+
+        Used to create new ressources
         """
 
         if not isinstance(data, dict):
@@ -68,7 +131,8 @@ class DataStore(object):
 
     def validate_fields(self, data):
         """
-        Validate some fields of the ressource
+        Validate only some fields of the ressource.
+        Used to update existing objects
         """
         if not isinstance(data, dict):
             raise BadRequest()
--- rest_api_framework/datastore/sql.py
+++ rest_api_framework/datastore/sql.py
@@ -4,6 +4,41 @@ import sqlite3
 
 
 class SQLiteDataStore(DataStore):
+    """
+    Define a sqlite datastore for your ressource.  you have to give
+    __init__ a data parameter containing the information to connect to
+    the database and to the table.
+
+    example:
+
+    .. code-block:: python
+
+       data={"table": "tweets",
+             "name": "test.db"}
+       options = {
+           'description': {
+               'message': {'type': str, 'required': True},
+               'id': {'type': 'autoincrement', 'required': True}
+       }
+       datastore = SQLiteDataStore(data, **options)
+
+    SQLiteDataStore implement a naive wrapper to convert description
+    types into database type.
+
+    * int will be saved in the database as INTEGER
+    * float will be saved in the database as REAL
+    * basestring will be saved in the database as TEXT
+    * autoincrement will be saved as INTEGER PRIMARY KEY AUTOINCREMENT
+
+    As soon as the datastore is instanciated, the database is create
+    if it does not exists and table is created too
+
+    .. note::
+
+       - Constrains are not supported for now
+       - It is not possible to use :memory database either.
+         The connection is closed after each operations
+    """
 
     wrapper = {int: "integer",
                float: "real",
@@ -28,14 +63,29 @@ class SQLiteDataStore(DataStore):
         conn.close()
 
     def get_connector(self):
+        """
+        return a sqlite3 connection to communicate with the table
+        define in self.table
+        """
         conn = sqlite3.connect(self.table)
         return conn
 
     def filter(self, **kwargs):
+        """
+        Change kwargs["query"] with "WHERE X=Y statements".  The
+        filtering will be done with the actual evaluation of the query
+        in :meth:`~.SQLiteDataStore.paginate` the sql can then be lazy
+        """
         kwargs['query'] += ' FROM {0}'
         return kwargs
 
     def paginate(self, data, **kwargs):
+        """
+        paginate the result of filter using ids limits.  Obviously, to
+        work properly, you have to set the start to the last ids you
+        receive from the last call on this method. The max number of
+        row this method can give back depend on the paginate_by option.
+        """
         args = []
         limit = self.options.get('paginate_by', None)
         if kwargs.get("start", None):
@@ -59,7 +109,8 @@ class SQLiteDataStore(DataStore):
 
     def get_list(self, **kwargs):
         """
-        return all the objects, paginated if needed
+        return all the objects, paginated if needed, fitered if
+        filters have been set.
         """
         fields = ", ".join([k for k in self.description.iterkeys()])
         kwargs["query"] = 'SELECT {0}'.format(fields)
@@ -69,6 +120,9 @@ class SQLiteDataStore(DataStore):
         return self.paginate(data, start=start, end=end)
 
     def get(self, identifier):
+        """
+        Return a single row or raise NotFound
+        """
         fields = ", ".join([k for k in self.description.iterkeys()])
         query = "select {0} from {1} where id=?".format(
             fields,
@@ -84,6 +138,10 @@ class SQLiteDataStore(DataStore):
         self.data['conn'].commit()
 
     def create(self, data):
+        """
+        Validate the data with :meth:`~.SQLiteDataStore.validate`
+        And, if data is valid, create the row in database and return it.
+        """
         self.validate(data)
         fields = []
         values = []
@@ -106,6 +164,13 @@ class SQLiteDataStore(DataStore):
         return c.lastrowid
 
     def update(self, obj, data):
+        """
+        Retreive the object to be updated
+        (:meth:`~.SQLiteDataStore.get` will raise a NotFound error if
+        the row does not exist)
+
+        Validate the fields to be updated and return the updated row
+        """
         self.get(obj['id'])
         self.validate_fields(data)
         fields = []
@@ -128,6 +193,13 @@ class SQLiteDataStore(DataStore):
         return self.get(obj['id'])
 
     def delete(self, identifier):
+        """
+        Retreive the object to be updated
+        (:meth:`~.SQLiteDataStore.get` will raise a NotFound error if
+        the row does not exist)
+
+        Return None
+        """
         self.get(identifier)
         conn = self.get_connector()
         c = conn.cursor()

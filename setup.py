3babdceda4aa31daf4b3f7b54113f9090965e80d
Yohann Gabory <yohann.gabory@outscale.com>
2013-10-21 08:22:30 +0200

    added the setup scritp

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Python REST API Framework',
+      version='0.1',
+      description='Python framework for REST API',
+      author='Yohann Gabory',
+      author_email='yohann@gabory.fr',
+      url='https://github.com/boblefrag/python-rest-api-framework',
+      packages=['rest_api_framework'],
+     )

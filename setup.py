f877cd4d7ea3401c35978d553ad0b00b8c75f817
yohann <yohann.gabory@novapost.fr>
2014-01-20 11:23:39 +0100

    updated the setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
 
 from distutils.core import setup
 
-setup(name='Python REST API Framework',
+setup(name='PRAF',
       version='0.1',
       description='Python framework for REST API',
       author='Yohann Gabory',
       author_email='yohann@gabory.fr',
       url='https://github.com/boblefrag/python-rest-api-framework',
-      packages=['rest_api_framework'],
-     )
+      packages=['rest_api_framework', 'rest_api_framework.datastore',
+                'rest_api_framework.models'],
+      install_requires=["werkzeug"]
+      )

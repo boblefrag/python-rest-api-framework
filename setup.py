2a74a60a24303c8c28ad0228c3d96649f67da4fa
yohann <yohann.gabory@novapost.fr>
2014-01-20 16:48:46 +0100

    added the version 1.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='PRAF',
-      version='0.1',
+      version='1.1',
       description='Python framework for REST API',
       author='Yohann Gabory',
       author_email='yohann@gabory.fr',
       url='https://github.com/boblefrag/python-rest-api-framework',
       packages=['rest_api_framework', 'rest_api_framework.datastore',
                 'rest_api_framework.models'],
-      install_requires=["werkzeug"]
+      install_requires=["werkzeug", "python-dateutil"]
       )
